0e0e53e8c54c368b86b00db896e2c8f45e386d7d
ELSHNKHLL <elshnkhll@yahoo.com>
2023-12-02 16:07:44 -0500

    Delete upload_macOS.sh

--- upload_macOS.sh
+++
@@ -1,2 +0,0 @@
-esptool --chip esp32 --port "/dev/cu.usbserial-01A8EF48" --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 s_RKTV_PL_XPRMNT_5.ino.bootloader.bin 0x8000 s_RKTV_PL_XPRMNT_5.ino.partitions.bin 0xe000 boot_app0.bin 0x10000 s_RKTV_PL_XPRMNT_5.ino.bin
-:                                              ^- put com port name here'
