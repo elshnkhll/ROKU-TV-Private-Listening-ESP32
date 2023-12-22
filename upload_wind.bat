67b3e92ad70e747fcae511eaf8347d6566640cee
ELSHNKHLL <elshnkhll@yahoo.com>
2023-12-22 17:15:05 -0500

    Delete upload_wind.bat

--- upload_wind.bat
+++
@@ -1,2 +0,0 @@
-esptool.exe --chip esp32 --port COM30 --baud 921600  --before default_reset --after hard_reset write_flash  -z --flash_mode dio --flash_freq 80m --flash_size 4MB 0x1000 s_RK_TV_PL_PRX_V5.h.bootloader.bin 0x8000 s_RK_TV_PL_PRX_V5.h.partitions.bin 0xe000 boot_app0.bin 0x10000 s_RK_TV_PL_PRX_V5.h.bin
-rem                                ^- put com port number here
